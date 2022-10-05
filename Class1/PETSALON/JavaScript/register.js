baeed99fefb679f267e8fd754d2a7e93f2000bf1
ELBASTIDAAS <75141067+ELBASTIDAAS@users.noreply.github.com>
2022-10-04 19:51:37 -0700

    register

--- Class1/PETSALON/JavaScript/register.js
+++ Class1/PETSALON/JavaScript/register.js
@@ -1,1 +1,38 @@
-console.log("Register script");
+//object literal {}
+let petSalon = {
+    //attributes
+    name:"The Fashion Pet",
+    phone: "666-666-666",
+    address:{
+        country:"Mex",
+        city:"Tijuana",
+        street:"Universidad",
+        number:"263-k",
+        zip:"22253"
+    },
+}
+
+let students =[
+    {
+        name:"Myles",
+        age:98,
+        grade:"A"
+    }, 
+    {
+        name:"Tuong",
+        age:99,
+        grade:"A-"
+    },
+    {
+        name:"Miguel",
+        age:97,
+        grade:"F"
+    }
+];//this is an array of students
+
+//FOR LOOP: star point; stopping condition ; increment
+//length return the number of elements in the array
+for (let i = 0; i < students.length; i++) {
+    console.log(students[i].age);
+}
+console.log("We have " + students.length + " students");
