502894b12a26c413e179edbe086daf438cac0159
Miguel Antonio Bastidas Verdin <miguel.bastidas18@tectijuana.edu.mx>
2022-10-06 20:40:53 -0700

    Class 2

--- Class1/PETSALON/JavaScript/register.js
+++ Class1/PETSALON/JavaScript/register.js
@@ -1,71 +1,59 @@
 //object literal {}
 let petSalon = {
     //attributes
-    name:"The Fashion Pet",
+    name: "The Fashion Pet",
     phone: "666-666-666",
-    address:{
-        country:"Mex",
-        city:"Tijuana",
-        street:"Universidad",
-        number:"263-k",
-        zip:"22253"
+    address: {
+        country: "Mex",
+        city: "Tijuana",
+        street: "Universidad",
+        number: "263-k",
+        zip: "22253"
     },
-    pets:[
-        {
-            name:"Dog",
-            age:"9 month",
-            gender:"Male",
-            service:"Consult"
-        },
-        {
-            name:"Cat",
-            age:"3 years",
-            gender:"Male",
-            service:"Consult"
-        },
-        {
-            name:"Bird",
-            age:"6 years",
-            gender:"Male",
-            service:"Consult"
-        }
-    ]
+    pets: []
 }
 
-function showregister(){
+//<------- this are the arguments ---->
+function Pet(name, age, gender, breed, service, owner, phone) {
+    //the real attributes are the next
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.breed = breed;
+    this.service = service;
+    this.ownerName = owner;
+    this.contactPhone = phone;
+}
 
+// create a new pet
+let scooby = new Pet("Scooby", 59, "Male", "Dane", "Grooming", "Shaggy", "777-777-777");
+let molly = new Pet("Molly", 45, "Female", "Dane", "Consult", "French", "666-555-777");
+let chimo = new Pet("Chimo", 59, "Male", "Dane", "Vaccine", "Husky", "777-777-777");
+// push the pet into the array
+petSalon.pets.push(scooby);
+petSalon.pets.push(molly);
+petSalon.pets.push(chimo);
+//register function
+let inputName = document.getElementById("txtName");
+let inputAge = document.getElementById("txtAge");
+let inputGender = document.getElementById("txtGender");
+let inputBreed = document.getElementById("txtBreed");
+let inputService = document.getElementById("txtService");
+function register() {
+    console.log(inputName.value);
+    let thePet = new Pet(inputName, inputAge.value, inputGender.value);
+    petSalon.pets.push(thePet);
+
+}
+function showRegister() {
     for (let i = 0; i < petSalon.pets.length; i++) {
-        alert([i+1] + " Animals Register");
+        console.log(petSalon.pets[i].name)
     }
+    alert(`we currently have ${petSalon.pets.length} registered.`)
 }
 
-function consoleshow(){
+function consoleshow() {
     for (let i = 0; i < petSalon.pets.length; i++) {
         console.log(petSalon.pets[i]);
     }
 }
-
-let students =[
-    {
-        name:"Myles",
-        age:98,
-        grade:"A"
-    }, 
-    {
-        name:"Tuong",
-        age:99,
-        grade:"A-"
-    },
-    {
-        name:"Miguel",
-        age:97,
-        grade:"F"
-    }
-];//this is an array of students
-
-//FOR LOOP: star point; stopping condition ; increment
-//length return the number of elements in the array
-for (let i = 0; i < students.length; i++) {
-    console.log(students[i].age);
-}
-console.log("We have " + students.length + " students");
